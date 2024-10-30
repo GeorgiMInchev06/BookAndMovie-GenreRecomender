4dd5a654e32d5807455b246852fcfc740e34d6f1
Georgi Minchev <georgiminchev1206@gmail.com>
2024-10-30 20:35:11 +0200

    Update Welcome page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,25 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth} from '@/app/firebase/config'
+import { useRouter } from 'next/navigation';
 
-export default function HomePage() {
+
+export default function WelcomePage() {
+
   return (
     <div className="relative h-screen w-screen bg-black">
-      <Link legacyBehavior href="/login">
-      <a className="absolute top-6 right-6 text-white text-lg z-20">Login</a>
-      </Link>
+      <Link href="/login" className="absolute top-6 right-6 text-white text-lg z-20" >Login</Link>
       <div className="absolute inset-0 z-0">
-      <Image
+      <Image 
           src="/images/WelcomeScreenBackgroun.png"
           alt="Background"
           layout="fill"
           objectFit="cover"
           quality={100}
+          className="object-center object-cover pointer-events-none"
         />
       </div>
       <div className="relative z-10 flex flex-col justify-center items-center h-full text-white">
@@ -22,11 +28,7 @@ export default function HomePage() {
           <span className="text-5xl">&</span>
           <span className="ml-4">Movie</span>
         </div>
-        <Link legacyBehavior href="/register">
-          <a className="bg-red-600 text-white py-3 px-12 rounded-md text-2xl hover:bg-red-700 transition duration-300">
-            Register
-          </a>
-        </Link>
+        <Link href="/register" className="bg-red-600 text-white py-3 px-12 rounded-md text-2xl hover:bg-red-700 transition duration-300">Register</Link>
       </div>
     </div>
   );
