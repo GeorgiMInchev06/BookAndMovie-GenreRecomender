f5920e97ca4d760a5360ce6374d70a6f3541092f
Georgi Minchev <georgiminchev1206@gmail.com>
2025-01-08 17:37:45 +0200

    Fetch data from tmdb api in the movies page.

--- src/app/home/movies/page.jsx
+++ src/app/home/movies/page.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Results from '@/components/Results';
+const MOVIES_API_KEY = process.env.MOVIES_API_KEY;
 
-export default function MoviesPage() {
+export default async function MoviesPage({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${MOVIES_API_KEY}&
+    language=en-US&page=1`
+  );
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  const results = data.results;
+
   return (
-    <div>Movies Page</div>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
