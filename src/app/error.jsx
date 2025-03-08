64fed91c6e160e101fa26c39e83d7cfe445955e0
Georgi Minchev <georgiminchev1206@gmail.com>
2025-03-08 19:15:08 +0200

    Updated error.jsx

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,12 +1,10 @@
 'use client';
-
-import React from 'react'
 import { useEffect } from 'react';
 
-export default function error({Error, reset}) {
+export default function Error({error, reset}) {
     useEffect(() => {
-        console.log(Error);
-    }, [Error]);
+        console.log(error);
+    }, [error]);
     return (
     <div className='text-center mt-10'>
         <h1>Something went wrong. Please try again later.</h1>
