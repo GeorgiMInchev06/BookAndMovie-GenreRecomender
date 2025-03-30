72bdac919cc460b1cf331abfacc2417ae6596a37
Georgi Minchev <georgiminchev1206@gmail.com>
2025-03-30 17:30:58 +0300

    Fixed image alignment in card component.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
 import Link from 'next/link';
-import Image from 'next/image';
 import { FiThumbsUp } from 'react-icons/fi';
 
 export default function Card({result}) {
   return (
     <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200'>
         <Link href={`/home/movies/movie/${result.id}`}>
-        <Image src={`https://image.tmdb.org/t/p/original/${
+        <img src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
         }`}
-        width={500}
-        height={300}
         alt= {result.title || result.name}
-        className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300'
-        ></Image>
+        className='sm:rounded-t-lg group-hover:opacity-75 transition-opacity duration-300  w-full sm:h-40'
+        ></img>
         <div className='p-2'>
           <p className='line-clamp-2 text-md'>{result.overview}</p>
           <h2 className='text-lg font-bold truncate'>{result.title || result.name}</h2>
